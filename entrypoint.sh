488028b8ab4a797842df338d0b8b600cfefb1c0b
Tftlegal <94695082+Tftlegal@users.noreply.github.com>
2025-01-25 22:12:53 +0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+#curl -s "https://get.sytes.ru/json/port_scan_results.json" | jq -r --arg ip "192.168.99.1" 'select(.ip? | index($ip)) | .open_ports| join(", ")'
+
+api_url="https://get.sytes.ru/json/port_scan_results.json"
+echo $api_url
+open_port=$(curl -s "${api_url}" | jq -r --arg ip "${INPUT_IP}" 'select(.ip? | index($ip)) | .open_ports| join(", ")')
+echo $open_port
+
+echo "::set-output name=open_port::$open_port"
+
+
